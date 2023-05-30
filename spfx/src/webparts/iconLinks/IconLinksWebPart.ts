762934c81e9ca62ec785c1f341dea531907d64a7
Gunjan Datta <gudatta@microsoft.com>
2023-05-30 11:14:51 -0400

    Created spfx folder.

---
+++ spfx/src/webparts/iconLinks/IconLinksWebPart.ts
@@ -0,0 +1,72 @@
+import { DisplayMode, Version } from '@microsoft/sp-core-library';
+import { IPropertyPaneConfiguration, PropertyPaneTextField } from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart, WebPartContext } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme, ISemanticColors } from '@microsoft/sp-component-base';
+import * as strings from 'IconLinksWebPartStrings';
+
+export interface IIconLinksWebPartProps {
+  viewName: string;
+  webUrl: string;
+}
+
+// Import the solution
+import "../../../../dist/icon-links.js";
+declare const IconLinks: {
+  render: new (el: HTMLElement, context: WebPartContext, displayMode: DisplayMode, viewName: string, sourceUrl: string) => void;
+  updateTheme: (currentTheme: Partial<ISemanticColors>) => void;
+};
+
+
+export default class IconLinksWebPart extends BaseClientSideWebPart<IIconLinksWebPartProps> {
+  private _hasRendered: boolean = false;
+
+  public render(): void {
+    // See if have rendered the solution
+    if (this._hasRendered) {
+      // Clear the element
+      while (this.domElement.firstChild) { this.domElement.removeChild(this.domElement.firstChild); }
+    }
+
+    // Render the solution
+    new IconLinks.render(this.domElement, this.context, this.displayMode, this.properties.viewName, this.properties.webUrl);
+
+    // Set the flag
+    this._hasRendered = true;
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    // Update the theme
+    IconLinks.updateTheme(currentTheme.semanticColors);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          groups: [
+            {
+              groupFields: [
+                PropertyPaneTextField('webUrl', {
+                  label: strings.WebUrlFieldLabel,
+                  description: strings.WebUrlFieldDescription
+                }),
+                PropertyPaneTextField('viewName', {
+                  label: strings.ViewNameFieldLabel,
+                  description: strings.ViewNameFieldDescription
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ spfx/src/webparts/iconLinks/loc/mystrings.d.ts
@@ -0,0 +1,20 @@
+declare interface IIconLinksWebPartStrings {
+  BasicGroupName: string;
+  ViewNameFieldDescription: string;
+  ViewNameFieldLabel: string;
+  WebUrlFieldDescription: string;
+  WebUrlFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'IconLinksWebPartStrings' {
+  const strings: IIconLinksWebPartStrings;
+  export = strings;
+}
